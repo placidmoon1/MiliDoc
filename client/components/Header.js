451bac3f09a30b3bd2d52a871a1649687fcd479d
Jin Young Bang <jybang@bu.edu>
2022-08-20 15:52:51 +0900

    Update styling

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,27 +1,45 @@
 import styles from "../styles/components/Header.module.css";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { MdDocumentScanner, MdUploadFile, MdSearch } from "react-icons/md";
+import { MdDocumentScanner, MdSearch } from "react-icons/md";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 const Header = () => {
   const router = useRouter();
 
   return (
     <header className={styles.container}>
-      <Image src="/logo.png" width={150} height={50} alt="어플 이름" onClick={() => router.push("/")} style={{marginLeft: "-10px", cursor: "pointer"}} />
-      <div className={styles.navigationContainer}>
+      <div className={styles.leftContainer}>
+        <Image
+          src="/logo.png"
+          width={150}
+          height={50}
+          alt="MiliDoc"
+          onClick={() => router.push("/")}
+          style={{ marginLeft: "-10px", cursor: "pointer" }}
+        />
         <div className={styles.iconTextView} style={{ marginRight: "15px" }}>
           <MdDocumentScanner style={{ marginRight: "4px" }} />
           <h2 onClick={() => router.push("/analyze")}>텍스트 분석</h2>
         </div>
-        {/* <div className={styles.iconTextView} style={{ marginRight: "15px" }}>
-          <MdUploadFile style={{ marginRight: "4px" }} />
-          <h2 onClick={() => router.push("/upload")}>파일 업로드</h2>
-        </div> */}
         <div className={styles.iconTextView}>
           <MdSearch style={{ marginRight: "4px" }} />
           <h2 onClick={() => router.push("/search")}>단어 검색</h2>
         </div>
+      </div>
+      <div className={styles.navigationContainer}>
+        <Button
+          onClick={() => router.push("/login")}
+          variant="ghost"
+        >
+          로그인
+        </Button>
+        <Button
+          onClick={() => router.push("/signup")}
+          variant="ghost"
+        >
+          회원가입
+        </Button>
       </div>
     </header>
   );
